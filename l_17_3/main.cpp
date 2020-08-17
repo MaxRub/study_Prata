2f08270041463ebd7a190c76891cb461928c5578
MaxRub <0633644209@i.ua>
2020-08-17 23:07:05 +0300

    This is exercise 3 of chapter 17 in C++ Primer Plus 6e by Stephen Prata

--- l_17_3/main.cpp
+++ l_17_3/main.cpp
@@ -1,9 +1,39 @@
 #include <iostream>
+#include <fstream>
+#include <cstdlib>
 
-using namespace std;
-
-int main()
+int main(int argc, char * argv[])
 {
+    using std::cout;
+    using std::cin;
+    using std::endl;
+    using std::ifstream;
+    using std::ofstream;
+
+    if (argc == 1)
+    {
+        std::cerr << "Usage: " << argv[0] << " filename[s]\n";
+        exit(EXIT_FAILURE);
+    }
+    ifstream fin(argv[1]);
+    ofstream fout(argv[2]);
+
+    if(fin.is_open())
+    {
+        while(fin)
+        {
+            char ch;
+            fin.get(ch);
+            fout << ch;
+        }
+    }
+    else
+    {
+        std::cerr << "Error in opening or reading " << argv[1] << ".\n";
+        exit(EXIT_FAILURE);
+    }
+    fin.close();
+    fout.close();
     cout << "Hello World!" << endl;
     return 0;
 }
